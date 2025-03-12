6832170a34f351fb694b3ba2a6244d77a9419fb8
Ivaylo Atanasov <aivo@devlabs.bg>
2025-03-12 09:49:32 +0200

    adds plugins migration

---
+++ storage/postgres/migrations/20250312074317_add_plugins.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS plugins (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    metadata JSONB NOT NULL,
+    server_endpoint VARCHAR(255) NOT NULL,
+    pricing_id INT NOT NULL,
+    CONSTRAINT fk_pricing FOREIGN KEY (pricing_id) REFERENCES pricings(id) ON DELETE SET NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS plugins;
+-- +goose StatementEnd
