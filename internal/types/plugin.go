2ca0ddeff28b097570b7f01ea6a793497b35b04a
Ivaylo Atanasov <aivo@devlabs.bg>
2025-03-14 09:16:08 +0200

    changes plugins required fields

--- internal/types/plugin.go
+++ internal/types/plugin.go
@@ -5,18 +5,18 @@ import "encoding/json"
 type Plugin struct {
 	ID             string          `json:"id" validate:"required"`
 	Title          string          `json:"title" validate:"required"`
-	Description    string          `json:"description"`
-	Metadata       json.RawMessage `json:"metadata"`
-	ServerEndpoint string          `json:"server_endpoint"`
-	PublicKey      string          `json:"public_key"`
-	PricingID      string          `json:"pricing_id"`
+	Description    string          `json:"description" validate:"required"`
+	Metadata       json.RawMessage `json:"metadata" validate:"required"`
+	ServerEndpoint string          `json:"server_endpoint" validate:"required"`
+	PublicKey      string          `json:"public_key" validate:"required"`
+	PricingID      string          `json:"pricing_id" validate:"required"`
 }
 
 type PluginCreateDto struct {
 	Title          string          `json:"title" validate:"required"`
-	Description    string          `json:"description"`
-	Metadata       json.RawMessage `json:"metadata"`
-	ServerEndpoint string          `json:"server_endpoint"`
-	PublicKey      string          `json:"public_key"`
-	PricingID      string          `json:"pricing_id"`
+	Description    string          `json:"description" validate:"required"`
+	Metadata       json.RawMessage `json:"metadata" validate:"required"`
+	ServerEndpoint string          `json:"server_endpoint" validate:"required"`
+	PublicKey      string          `json:"public_key" validate:"required"`
+	PricingID      string          `json:"pricing_id" validate:"required"`
 }
