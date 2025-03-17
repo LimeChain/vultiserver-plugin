b6c84784e094cef84072b8695b67143384f58c93
Ivaylo Atanasov <aivo@devlabs.bg>
2025-03-17 15:08:12 +0200

    removes public_key feld as it would be a relation

--- storage/postgres/migrations/20250312074317_add_plugins.sql
+++ storage/postgres/migrations/20250312074317_add_plugins.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS plugins (
     description TEXT NOT NULL,
     metadata JSONB NOT NULL,
     server_endpoint VARCHAR(255) NOT NULL,
-    public_key VARCHAR(255) NOT NULL,
     pricing_id INT NOT NULL,
     CONSTRAINT fk_pricing FOREIGN KEY (pricing_id) REFERENCES pricings(id) ON DELETE SET NULL
 );
