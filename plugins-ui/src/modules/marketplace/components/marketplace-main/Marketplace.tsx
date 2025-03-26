2478553da8aaca14e6ef0d4d12bde1bf8e8b9371
ivalime <iva.zhelyazkova@limechain.tech>
2025-03-26 15:46:38 +0200

    add pagination & sorting (#64)

--- plugins-ui/src/modules/marketplace/components/marketplace-main/Marketplace.tsx
+++ plugins-ui/src/modules/marketplace/components/marketplace-main/Marketplace.tsx
@@ -4,17 +4,23 @@ import { useNavigate } from "react-router-dom";
 import "./Marketplace.css";
 import MarketplaceFilters from "../marketplace-filters/MarketplaceFilters";
 import { useEffect, useState } from "react";
-import { Plugin, ViewFilter } from "../../models/marketplace";
+import { PluginMap, ViewFilter } from "../../models/marketplace";
 import Toast from "@/modules/core/components/ui/toast/Toast";
 import MarketplaceService from "../../services/marketplaceService";
+import Pagination from "@/modules/core/components/ui/pagination/Pagination";
 
 const getSavedView = (): string => {
   return localStorage.getItem("view") || "grid";
 };
 
+const ITEMS_PER_PAGE = 6;
+
 const Marketplace = () => {
   const navigate = useNavigate();
   const [view, setView] = useState<string>(getSavedView());
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   const changeView = (view: ViewFilter) => {
     localStorage.setItem("view", view);
@@ -27,14 +33,24 @@ const Marketplace = () => {
     type: "success" | "error";
   } | null>(null);
 
-  const [plugins, setPlugins] = useState<Plugin[] | null>(null);
+  const [pluginsMap, setPlugins] = useState<PluginMap | null>(null);
 
   useEffect(() => {
     const fetchPlugins = async (): Promise<void> => {
       try {
-        const fetchedPlugins = await MarketplaceService.getPlugins();
-        console.log("fetchedPlugins", fetchedPlugins);
+        const fetchedPlugins = await MarketplaceService.getPlugins(
+          currentPage > 1 ? (currentPage - 1) * ITEMS_PER_PAGE : 0,
+          ITEMS_PER_PAGE
+        );
         setPlugins(fetchedPlugins);
+        setTotalPages(Math.ceil(fetchedPlugins.total_count / ITEMS_PER_PAGE));
+
+        if (
+          fetchedPlugins.total_count / ITEMS_PER_PAGE > 1 &&
+          currentPage === 0
+        ) {
+          setCurrentPage(1);
+        }
       } catch (error: any) {
         console.error("Failed to get plugins:", error.message);
         setToast({
@@ -46,11 +62,15 @@ const Marketplace = () => {
     };
 
     fetchPlugins();
-  }, []);
+  }, [currentPage]);
+
+  const onCurrentPageChange = (page: number): void => {
+    setCurrentPage(page);
+  };
 
   return (
     <>
-      {plugins && (
+      {pluginsMap && (
         <div className="only-section">
           <h2>Plugins Marketplace</h2>
           <MarketplaceFilters
@@ -58,7 +78,7 @@ const Marketplace = () => {
             onChange={changeView}
           />
           <section className="cards">
-            {plugins.map((plugin) => (
+            {pluginsMap.plugins?.map((plugin) => (
               <div
                 className={view === "list" ? "list-card" : ""}
                 key={plugin.id}
@@ -73,6 +93,14 @@ const Marketplace = () => {
               </div>
             ))}
           </section>
+
+          {totalPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={onCurrentPageChange}
+            />
+          )}
 
           <Button
             size="small"
