2d6894fcfffa3e546b04f9729ba334b002e5da5d
Misho <mihail.genchev@limechain.tech>
2025-04-09 16:25:48 +0300

    return slippage to 1 and fixed unit tests

--- plugin/dca/dca_test.go
+++ plugin/dca/dca_test.go
@@ -559,7 +559,7 @@ func TestProposeTransactions(t *testing.T) {
 				uniswap.On("GetExpectedAmountOut", mock.Anything, mock.Anything).
 					Return(big.NewInt(90), nil)
 
-				uniswap.On("CalculateAmountOutMin", big.NewInt(90), 1.0).
+				uniswap.On("CalculateAmountOutMin", big.NewInt(90)).
 					Return(big.NewInt(89))
 
 				uniswap.On("SwapTokens",
@@ -584,7 +584,7 @@ func TestProposeTransactions(t *testing.T) {
 				uniswap.On("GetExpectedAmountOut", mock.Anything, mock.Anything).
 					Return(big.NewInt(90), nil)
 
-				uniswap.On("CalculateAmountOutMin", big.NewInt(90), 1.0).
+				uniswap.On("CalculateAmountOutMin", big.NewInt(90)).
 					Return(big.NewInt(89))
 
 				uniswap.On("SwapTokens",
